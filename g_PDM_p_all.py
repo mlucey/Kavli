baef3e3f3249173e4e4a06bd6c8a5edf4435437b
Maddie Lucey <40609462+mlucey@users.noreply.github.com>
2019-09-09 13:38:41 -0500

    Add files via upload

--- g_PDM_p_all.py
+++ g_PDM_p_all.py
@@ -162,10 +162,7 @@ def GenData_lamost(fileIn = 'lamost_wise_gaia_PS1_2mass.fits'):
       # color mag
 
 
-    y_train = y_train_shuffled[:num_train]  # spec z
-    y_test = y_train_shuffled[num_train + 1: num_train + num_test]  # spec z
-    #classy = classy_shuffled[:num_train]
-    params = params_shuffled[:num_train]
+
     inds_train = np.where(al['snrg'][TrainshuffleOrder]>50)[0][:num_train]
     train_tinds = tinds[inds_train]
 
@@ -173,6 +170,10 @@ def GenData_lamost(fileIn = 'lamost_wise_gaia_PS1_2mass.fits'):
 
     X_train = x_train_shuffled[inds_train]  # color mag
     X_test = x_train_shuffled[test_tinds][:num_test]
+    y_train = y_train_shuffled[inds_train]  # spec z
+    y_test = y_train_shuffled[test_inds][: num_test]  # spec z
+    #classy = classy_shuffled[:num_train]
+    params = params_shuffled[inds_train]
     test_tinds = tinds[test_tinds][:num_test]
     # return (X_train[:, 2:8], y_train, X_test[:, 2:8], y_test)
     # return (X_train[:, :5], y_train[:, 0], X_test[:, :5], y_test[:, 0])
